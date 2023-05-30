e10ca3ce864c9d8e44e07e06ac674e675ff698d9
JakariaJishan <jakariajishan987@gmail.com>
2023-05-30 21:26:29 +0600

    List foods and add form for adding food

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,3 +1,25 @@
 class FoodsController < ApplicationController
-  def index; end
+  def index
+    @foods = Food.all
+  end
+
+  def new
+    @food = Food.new
+  end
+
+  def create
+    @food = Food.new(food_params)
+    if @food.save
+      redirect_to foods_path, notice:"Food was successfully created"
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def food_params
+    params.require(:food).permit(:name, :measurement_unit, :price)
+  end
 end
