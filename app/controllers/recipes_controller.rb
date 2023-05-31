a53ae72ddebec9a7ed143d91f3e0813fb9d7fb75
JakariaJishan <jakariajishan987@gmail.com>
2023-05-31 22:41:44 +0600

    Implement recipe details for public recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,7 +1,9 @@
 class RecipesController < ApplicationController
+# load_and_authorize_resource
   def index; end
 
   def show
-    render :show
+    @recipes = Recipe.find(params[:id])
+    @recipe_foods = RecipeFood.includes(:food).find(params[:id])
   end
 end
