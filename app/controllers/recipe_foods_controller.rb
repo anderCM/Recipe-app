e3bc834c9686bcd10b1e941e7afcf5bc64824ca8
anderCM <andercm15@gmail.com>
2023-05-31 04:57:05 -0500

    n+1 solved and nested request to DB inside .html.erb fixed

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -1,5 +1,5 @@
 class RecipeFoodsController < ApplicationController
   def index
-    @recipe_names = Recipe.includes(:user).where(public: true).order(created_at: :desc)
+    @recipe_names = Recipe.includes(:user, recipe_foods: :food).where(public: true).order(created_at: :desc)
   end
 end
