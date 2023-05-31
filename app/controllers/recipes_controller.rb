048cb8ce0236609301bfd6fe8cbb8d5076fa69c4
anderCM <andercm15@gmail.com>
2023-05-31 12:32:05 -0500

    create, index and new methos for Recipes added

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,7 +1,32 @@
 class RecipesController < ApplicationController
-  def index; end
+  before_action :authenticate_user!
+  load_and_authorize_resource
+  def index
+    @recipes = Recipe.where(user: current_user)
+  end
+
+  def new
+    @recipes = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    @recipe.user = current_user
+
+    if @recipe.save
+      redirect_to recipes_path, notice: 'Recipe was successfully created'
+    else
+      render :new
+    end
+  end
 
   def show
     render :show
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description)
   end
 end
