4f91b97d3ad7827405bce89d352c560d4182b370
anderCM <andercm15@gmail.com>
2023-06-01 12:41:14 -0500

    Ability updated to read only public recipes

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,10 +4,11 @@ class Ability
   def initialize(user)
     can :read, RecipeFood
     can :read, Food
-    can :read, Recipe
+    can :read, Recipe do |recipe|
+      recipe.public == true
+    end
     return unless user.present?
 
     can(:manage, :all, user:)
-    can :read, Recipe
   end
 end
