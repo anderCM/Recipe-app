e95ef5269322829504692bd7df14bd3b4c90a69a
anderCM <andercm15@gmail.com>
2023-06-01 18:11:12 -0500

    Specs for Food model added

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,6 +1,10 @@
 class Food < ApplicationRecord
   validates :name, presence: true
   validates :quantity, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
+  validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
+  validates :measurement_unit, presence: true
+
+  attribute :quantity, default: 1
 
   belongs_to :user
   has_many :recipe_foods, dependent: :destroy
