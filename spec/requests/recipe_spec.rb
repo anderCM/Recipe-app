f33ca4d30430257940ba45174e66166d567d1cd2
anderCM <andercm15@gmail.com>
2023-06-02 14:12:18 -0500

    show method for Recipes controller spec added

--- spec/requests/recipe_spec.rb
+++ spec/requests/recipe_spec.rb
@@ -40,7 +40,7 @@ RSpec.describe 'Recipe', type: :request do
       expect(assigns(:recipes)).to match_array([@recipe])
     end
 
-    it 'Should accept params and increase recipes counter' do
+    it 'Should accept params, increase recipes counter and redirect' do
       recipe_params = {
         name: 'soupe al onion',
         preparation_time: 30,
@@ -50,6 +50,37 @@ RSpec.describe 'Recipe', type: :request do
       }
       expect { post recipes_path, params: { recipe: recipe_params } }.to change(Recipe, :count)
       expect(response).to redirect_to(recipes_path)
+    end
+
+    it 'Should accept params, increase RecipeFood counter and redirect' do
+      food_params = {
+        name: 'soupe al onion',
+        measurement_unit: 'kgs',
+        price: 1,
+        quantity: 1
+      }
+
+      expect { post add_ingredient_recipe_path(@recipe), params: { food: food_params } }.to change(RecipeFood, :count)
+      expect(response).to redirect_to(recipe_path(@recipe))
+    end
+
+    it 'should display food inside recipe' do
+      food = Food.create(name: Faker::Food.unique.ingredient,
+                    measurement_unit: Faker::Food.measurement,
+                    price: Faker::Number.decimal(l_digits: 2),
+                    quantity: Faker::Number.between(from: 1, to: 10),
+                    user: @user)
+
+      food2 = Food.create(name: Faker::Food.unique.ingredient,
+                    measurement_unit: Faker::Food.measurement,
+                    price: Faker::Number.decimal(l_digits: 2),
+                    quantity: Faker::Number.between(from: 1, to: 10),
+                    user: @user)
+      RecipeFood.create(recipe: @recipe, food: , quantity: 1)
+      RecipeFood.create(recipe: @recipe, food: food2, quantity: 1)
+
+      get recipe_path(@recipe)
+      expect(assigns(:recipe_foods)).to match_array([recipe_food1, recipe_food2])
     end
   end
 end
