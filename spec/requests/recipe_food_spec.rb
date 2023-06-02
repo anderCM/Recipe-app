a8e734d76f679a7fdf4737c27bf1e245a733ed94
JakariaJishan <jakariajishan987@gmail.com>
2023-06-02 23:51:48 +0600

    Add test case for food view

--- spec/requests/recipe_food_spec.rb
+++ spec/requests/recipe_food_spec.rb
@@ -3,20 +3,12 @@ require 'faker'
 
 RSpec.describe 'Recipe Food', type: :request do
   describe 'GET #index' do
-
     before do
       @user = User.create(
         name: Faker::Name.first_name,
         email: Faker::Internet.email,
         password: Faker::Internet.password
       )
-
-      sign_in @user
-
-      @name = Faker::Food.unique.ingredient
-      @measurement_unit = Faker::Food.measurement
-      @price = Faker::Number.number(digits: 10)
-      @quantity = Faker::Number.between(from: 1, to: 10)
 
       @recipe = Recipe.create(name: Faker::Food.dish,
                               preparation_time: Faker::Time.between(from: DateTime.now,
@@ -43,8 +35,7 @@ RSpec.describe 'Recipe Food', type: :request do
     end
 
     it 'Should include new recipe' do
-        expect(assigns(:recipe_names)).to include(@recipe)
+      expect(assigns(:recipe_names)).to include(@recipe)
     end
-    
   end
 end
